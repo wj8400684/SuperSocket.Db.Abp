25b96f21ea25cb19125763636e36f4533183d3c3
吴俊 <8400684@qq.com>
2023-04-28 23:52:38 +0800

    添加说明添加说明

--- SuperSocket.Db.Abp.Core/Packages/LoginPackage.cs
+++ SuperSocket.Db.Abp.Core/Packages/LoginPackage.cs
@@ -13,6 +13,11 @@ public sealed class LoginPackage : MyPackage
 
     public string? Password { get; set; }
 
+    /// <summary>
+    /// 这里可以用一些常用序列化框架如 protobuf messagePack memoryPack 可以切换至 protobuf分支
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <returns></returns>
     public override int Encode(IBufferWriter<byte> writer)
     {
         ArgumentException.ThrowIfNullOrEmpty(Username);
@@ -24,6 +29,11 @@ public sealed class LoginPackage : MyPackage
         return length;
     }
 
+    /// <summary>
+    /// 这里可以用一些常用序列化框架如 protobuf messagePack memoryPack 可以切换至 protobuf分支
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="context"></param>
     protected internal override void DecodeBody(ref SequenceReader<byte> reader, object? context)
     {
         if (reader.TryReadEncoderString(out var username))
