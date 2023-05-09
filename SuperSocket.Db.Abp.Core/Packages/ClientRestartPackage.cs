af642a3999d82c7877c8babcec7207a443021d6e
吴俊 <8400684@qq.com>
2023-05-09 14:22:04 +0800

    优化

---
+++ SuperSocket.Db.Abp.Core/Packages/ClientRestartPackage.cs
@@ -0,0 +1,36 @@
+﻿using MemoryPack;
+using System.Buffers;
+
+namespace SuperSocket.Db.Abp.Core.Packages;
+
+[MemoryPackable]
+public sealed partial class ClientRestartPackage : MyPackage
+{
+    public ClientRestartPackage() : base(MyCommand.ClientRestart)
+    {
+    }
+
+    public string? Id { get; set; }
+
+
+    public override int Encode(IBufferWriter<byte> writer)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(Id);
+
+        return base.Encode(writer);
+    }
+
+    public override void Dispose()
+    {
+        Id = default;
+        base.Dispose();
+    }
+}
+
+[MemoryPackable]
+public sealed partial class ClientRestartRespPackage : MyRespPackage
+{
+    public ClientRestartPackage() : base(MyCommand.ClientRestartAck)
+    {
+    }
+}
---
+++ SuperSocket.Db.Abp.Core/Packages/OrderAddPackage.cs
@@ -0,0 +1,36 @@
+﻿using MemoryPack;
+using System.Buffers;
+
+namespace SuperSocket.Db.Abp.Core.Packages;
+
+[MemoryPackable]
+public sealed partial class OrderAddPackage : MyPackage
+{
+    public OrderAddPackage() : base(MyCommand.OrderAdd)
+    {
+    }
+
+    public string? Id { get; set; }
+
+
+    public override int Encode(IBufferWriter<byte> writer)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(Id);
+
+        return base.Encode(writer);
+    }
+
+    public override void Dispose()
+    {
+        Id = default;
+        base.Dispose();
+    }
+}
+
+[MemoryPackable]
+public sealed partial class OrderAddRespPackage : MyRespPackage
+{
+    public OrderAddRespPackage() : base(MyCommand.OrderAddAck)
+    {
+    }
+}
